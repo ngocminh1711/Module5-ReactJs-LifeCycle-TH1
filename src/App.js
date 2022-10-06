f646bbcf928d829fef86eff03e25e04e47e8a4aa
ngo minh ngoc <ngoc.ngominh1711@gmail.com>
2022-10-06 20:48:56 +0700

    upfile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+
+const numbers = {
+    data: 0
+}
+
+    function NumberDecreaseInCrease({myNumber,onClickDecrease, onClickIncrease}){
+        return (
+            <div>
+                <button onClick={onClickDecrease}>Decrease</button>
+                {myNumber}
+                <button onClick={onClickIncrease}>Increase</button>
+
+            </div>
+        )
+    }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const handleDecrease = () => {
+        setNumber({data: number.data + -1})
+    }
+    const handleIncrease = () => {
+        setNumber({data: number.data + 1})
+    }
+    const [number, setNumber] = useState(numbers)
+        return (
+            <div>
+                <NumberDecreaseInCrease
+                    onClickDecrease={handleDecrease}
+                    onClickIncrease={handleIncrease}
+                    myNumber={number.data}
+                />
+            </div>
+
+        )
 }
 
 export default App;
